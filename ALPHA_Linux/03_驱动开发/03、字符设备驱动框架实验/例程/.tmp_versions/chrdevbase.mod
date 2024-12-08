5913853edd7857acf99be36c17aaf1e44994164c
wqh <2361125846@qq.com>
2024-12-08 22:36:53 +0800

    完成3.2讲

---
+++ ALPHA_Linux/03_驱动开发/03、字符设备驱动框架实验/例程/.tmp_versions/chrdevbase.mod
@@ -0,0 +1,2 @@
+/home/wqh/Project/Linux/ALPHA_Linux/03_驱动开发/03、字符设备驱动框架实验/例程/chrdevbase.ko
+/home/wqh/Project/Linux/ALPHA_Linux/03_驱动开发/03、字符设备驱动框架实验/例程/chrdevbase.o
--- ALPHA_Linux/03_驱动开发/03、字符设备驱动框架实验/例程/chrdevbase.c
+++ ALPHA_Linux/03_驱动开发/03、字符设备驱动框架实验/例程/chrdevbase.c
@@ -1,11 +1,14 @@
 #include "linux/module.h"
+#include "linux/kernel.h"
+#include "linux/init.h"
 
 static int __init chrdevbase_init(void) {
+    printk("chrdevbase_init\r\n");
     return 0;
 }
 
 static void __exit chrdevbase_exit(void) {
-
+    printk("chrdevbase_exit\r\n");
 }
 
 
@@ -14,5 +17,6 @@ module_init(chrdevbase_init);      // 入口
 module_exit(chrdevbase_exit);      // 出口
 
 
-
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("WangQinghao");
 
